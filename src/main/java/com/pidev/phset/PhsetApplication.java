ec49c54549050f1b08d04c280bf20a37240e8d37
Achref Zitoun <achref.zitoun@esprit.tn>
2023-02-19 18:59:11 +0100

    Initial project

---
+++ src/main/java/com/pidev/phset/PhsetApplication.java
@@ -0,0 +1,13 @@
+package com.pidev.phset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhsetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhsetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pidev/phset/PhsetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pidev.phset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhsetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
