e7bc0ba7a74906a443c314bec6ce36274ada8272
Achref Zitoun <92675276+achrefzitoun@users.noreply.github.com>
2023-03-14 11:52:01 +0100

    Update Account.java

--- src/main/java/com/pidev/phset/entities/Account.java
+++ src/main/java/com/pidev/phset/entities/Account.java
@@ -1,22 +1,15 @@
 package com.pidev.phset.entities;
 
-import java.io.Serializable;
-import java.util.Set;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import javafx.geometry.Pos;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -30,37 +23,47 @@ public class Account implements Serializable {
     private String emailAccount;
     private String passwordAcccount;
     @OneToOne
+            @JsonIgnore
     User user;
 
     @OneToMany(mappedBy = "account")
+    @JsonIgnore
+
     Set<Reservation> reservations;
 
     @ManyToOne
+    @JsonIgnore
     Team team;
 
     @OneToMany(mappedBy = "account")
+    @JsonIgnore
     Set<Claim> claims;
 
     @OneToMany(mappedBy = "account")
+    @JsonIgnore
     Set<Post> posts;
 
     @OneToMany(mappedBy = "account")
+    @JsonIgnore
     Set<Publicity> publicities;
 
     @OneToOne(mappedBy = "account")
+    @JsonIgnore
     Inscription inscription;
 
     @OneToOne(mappedBy = "account")
+    @JsonIgnore
     Badge badge;
 
     @ManyToMany(mappedBy = "accounts")
+    @JsonIgnore
     Set<Training> trainings;
 
     @OneToMany(mappedBy = "account")
+    @JsonIgnore
     Set<QuestionFAQ> questionFAQ;
 
-    @ManyToMany(mappedBy = "accounts")
-    Set<Interview> interviews;
-    @OneToMany(cascade = CascadeType.ALL)
-    Set<Post> post;
+
+    @OneToMany(mappedBy = "account")
+    Set<Certificate> certificates;
 }
